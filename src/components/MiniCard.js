e32db2b5a7c4afa4dae0d6559a67e3809187bc9a
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-04-27 15:57:12 +0700

    Fix Image

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -9,7 +9,7 @@ const MiniCard = (props) => {
             marginBottom:0
         }}>
             <Image 
-            source={{uri:`https://i.ytimg.com/vi/${props.videoId}/maxresdefault.jpg`}}
+            source={{uri:`https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`}}
             style={{
                 width:"45%",
                 height:100
