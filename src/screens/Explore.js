103426dba65851bc74888239a84120aa3d56d289
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-05-04 11:18:27 +0700

    Add Explore Screen

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
 import Header from '../components/Header';
+import Card from '../components/Card';
+import {useSelector} from 'react-redux';
+
+const LittleCard = ({name}) => {
+    return(
+        <View style={{
+            backgroundColor:"red",
+            hight:50,
+            width:160,
+            borderRadius:4,
+            marginTop:10
+        }}>
+            <Text style={{
+                textAlign:"center",
+                color:"white",
+                fontSize:22,
+                marginVertical:5
+            }}>{name}</Text>
+        </View>
+    )
+}
 
 const Explore = () => {
+    const cardData = useSelector(state=>{
+        return state
+      })
     return(
     <View style={{flex:1}}>
         <Header />
-        <Text>Explore</Text>
+        <ScrollView>
+        <View style={{
+            flexDirection:"row",
+            flexWrap:"wrap",
+            justifyContent:"space-around"
+        }}>
+            <LittleCard name="Gaming"/>
+            <LittleCard name="Music"/>
+            <LittleCard name="Anime"/>
+            <LittleCard name="Trending"/>
+            <LittleCard name="News"/>
+            <LittleCard name="Nature"/>
+        </View>
+        <Text style={{
+            margin:8,
+            fontSize:22,
+            borderBottomWidth:1
+        }}>
+            Trending Videos
+        </Text>
+        <FlatList
+        data={cardData}
+        renderItem={({item})=>{
+            return <Card 
+            videoId={item.id.videoId}
+            title={item.snippet.title}
+            channel={item.snippet.channelTitle}
+            />
+        }}
+        keyExtractor={item=>item.id.videoId}
+        />
+        </ScrollView>
     </View>
     )
 }
